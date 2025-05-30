17eba666fe70f9d4aaefdcd3277409478e81cda0
Talentum GameDev 13 <143554849+talentumgamedev13@users.noreply.github.com>
2025-05-30 15:08:52 -0500

    Tailwind init

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <p className='bg-red-500'>AAAA</p>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
