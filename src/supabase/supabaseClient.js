91b38a252457dc4dff974bff1e06f7b2802a0597
Talentum GameDev 13 <museortiz@gmail.com>
2025-06-06 16:38:34 -0500

    fix import env

--- src/supabase/supabaseClient.js
+++ src/supabase/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPA_PROJECT;
-const supabaseAnonKey = process.env.ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  import.meta.env.VITE_SUPA_PROJECT,
+  import.meta.env.VITE_ANON_KEY
+)
